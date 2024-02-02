b629bf3157e90eb7ed2e1d7bf3011fd9198d85fe
Takuya Aso <milanista224@gmail.com>
2024-02-02 13:45:00 +0900

    Initial Commit

---
+++ ScrollDetectSample/ScrollDetectSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollDetectSampleApp.swift
+//  ScrollDetectSample
+//
+//  Created by Takuya Aso on 2024/02/02.
+//
+
+import SwiftUI
+
+@main
+struct ScrollDetectSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
